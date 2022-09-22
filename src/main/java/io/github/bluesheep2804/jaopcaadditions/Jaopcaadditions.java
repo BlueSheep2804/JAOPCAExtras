2700ab7dd223e1b20b7432226f3e87426ccdc87a
BlueSheep2804 <48321445+BlueSheep2804@users.noreply.github.com>
2022-09-22 18:53:15 +0900

    first commit

---
+++ src/main/java/io/github/bluesheep2804/jaopcaadditions/Jaopcaadditions.java
@@ -0,0 +1,8 @@
+package io.github.bluesheep2804.jaopcaadditions;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Jaopcaadditions.MOD_ID)
+public class Jaopcaadditions {
+    public static final String MOD_ID = "jaopcaadditions";
+}
