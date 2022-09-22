124b2f9b143ffc6e508e6ff2140134a42033cac1
BlueSheep2804 <48321445+BlueSheep2804@users.noreply.github.com>
2022-09-22 19:19:14 +0900

    feat: 🎸 add Plates

---
+++ src/main/java/io/github/bluesheep2804/jaopcaadditions/modules/PlatesModule.java
@@ -0,0 +1,65 @@
+package io.github.bluesheep2804.jaopcaadditions.modules;
+
+import com.mojang.logging.LogUtils;
+import io.github.bluesheep2804.jaopcaadditions.recipes.ShapedRecipeSerializer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.slf4j.Logger;
+import thelm.jaopca.api.JAOPCAApi;
+import thelm.jaopca.api.forms.IForm;
+import thelm.jaopca.api.forms.IFormRequest;
+import thelm.jaopca.api.helpers.IMiscHelper;
+import thelm.jaopca.api.items.IItemInfo;
+import thelm.jaopca.api.materials.IMaterial;
+import thelm.jaopca.api.materials.MaterialType;
+import thelm.jaopca.api.modules.IModule;
+import thelm.jaopca.api.modules.IModuleData;
+import thelm.jaopca.api.modules.JAOPCAModule;
+
+import java.util.*;
+
+@JAOPCAModule
+public class PlatesModule implements IModule {
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    private final JAOPCAApi api = JAOPCAApi.instance();
+    private final IForm plateForm = api.newForm(this, "additions_plates", api.itemFormType()).setMaterialTypes(MaterialType.NON_DUSTS);
+
+    @Override
+    public String getName() {
+        return "plates";
+    }
+
+    @Override
+    public Set<MaterialType> getMaterialTypes() {
+        return EnumSet.allOf(MaterialType.class);
+    }
+
+    @Override
+    public List<IFormRequest> getFormRequests() {
+        return Collections.singletonList(this.api.newFormRequest(this, this.plateForm));
+    }
+
+    @Override
+    public void onCommonSetup(IModuleData moduleData, FMLCommonSetupEvent event) {
+        IMiscHelper miscHelper = api.miscHelper();
+        for (IMaterial material : plateForm.getMaterials()) {
+            ResourceLocation materialLocation = miscHelper.getTagLocation(material.getType().getFormName(), material.getName());
+            IItemInfo plateInfo = api.itemFormType().getMaterialFormInfo(plateForm, material);
+            api.registerRecipe(
+                    new ResourceLocation("jaopcaadditions", "plates.from_material." + material.getName()),
+                    new ShapedRecipeSerializer(
+                            new String[]{
+                                    "M ",
+                                    " M"
+                            },
+                            Map.of(
+                                    "M", materialLocation
+                            )
+                            ,plateInfo,1
+                    )
+            );
+        }
+    }
+}
---
+++ src/main/java/io/github/bluesheep2804/jaopcaadditions/recipes/ShapedRecipeSerializer.java
@@ -0,0 +1,56 @@
+package io.github.bluesheep2804.jaopcaadditions.recipes;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import thelm.jaopca.api.JAOPCAApi;
+import thelm.jaopca.api.helpers.IMiscHelper;
+import thelm.jaopca.api.recipes.IRecipeSerializer;
+
+import java.util.Map;
+
+public class ShapedRecipeSerializer implements IRecipeSerializer {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public final String[] pattern;
+    public final Map<String, ResourceLocation> key;
+    public final Object output;
+    public final Integer count;
+
+    public ShapedRecipeSerializer(String[] pattern, Map<String, ResourceLocation> key, Object output, Integer count) {
+        this.pattern = pattern;
+        this.key = key;
+        this.output = output;
+        this.count = count;
+    }
+
+    @Override
+    public JsonElement get() {
+        JAOPCAApi api = JAOPCAApi.instance();
+        IMiscHelper miscHelper = api.miscHelper();
+        JsonObject json = new JsonObject();
+        json.addProperty("type", "minecraft:crafting_shaped");
+
+        JsonArray patternJson = new JsonArray();
+        for (String str : this.pattern) {
+            patternJson.add(str);
+        }
+        json.add("pattern", patternJson);
+
+        JsonObject keyJson = new JsonObject();
+        for (Map.Entry<String, ResourceLocation> entry : this.key.entrySet()) {
+            keyJson.add(String.valueOf(entry.getKey()), miscHelper.getIngredient(entry.getValue()).toJson());
+        }
+        json.add("key", keyJson);
+
+        ItemStack stack = miscHelper.getItemStack(this.output, this.count);
+        json.add("result", miscHelper.serializeItemStack(stack));
+
+        LOGGER.info(json);
+        return json;
+    }
+}
