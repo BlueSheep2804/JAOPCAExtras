85494349a379efb241a295de6cf41292077f82c5
BlueSheep2804 <48321445+BlueSheep2804@users.noreply.github.com>
2022-12-11 18:39:45 +0900

    feat: 🎸 add circuits and processor by AE2

---
+++ src/main/java/io/github/bluesheep2804/jaopcaextras/modules/AE2Module.java
@@ -0,0 +1,92 @@
+package io.github.bluesheep2804.jaopcaextras.modules;
+
+import com.mojang.logging.LogUtils;
+import io.github.bluesheep2804.jaopcaextras.recipes.AE2InscriberRecipeSerializer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+import thelm.jaopca.api.JAOPCAApi;
+import thelm.jaopca.api.forms.IForm;
+import thelm.jaopca.api.forms.IFormRequest;
+import thelm.jaopca.api.helpers.IMiscHelper;
+import thelm.jaopca.api.items.IItemInfo;
+import thelm.jaopca.api.materials.IMaterial;
+import thelm.jaopca.api.materials.MaterialType;
+import thelm.jaopca.api.modules.IModule;
+import thelm.jaopca.api.modules.IModuleData;
+import thelm.jaopca.api.modules.JAOPCAModule;
+
+import java.util.*;
+
+@JAOPCAModule(modDependencies = "ae2")
+public class AE2Module implements IModule {
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    private final JAOPCAApi api = JAOPCAApi.instance();
+    private static final Set<String> BLACKLIST = new TreeSet<>(Arrays.asList(
+            "brick", "nether_brick",
+            "prismarine", "sulfur",
+            "diamond", "certus_quartz", "gold", "resonating"
+    ));
+    private final IForm processorForm = api.newForm(this, "processors", api.itemFormType()).setMaterialTypes(MaterialType.NON_DUSTS).setDefaultMaterialBlacklist(BLACKLIST);
+    private final IForm circuitForm = api.newForm(this, "circuits", api.itemFormType()).setMaterialTypes(MaterialType.NON_DUSTS).setDefaultMaterialBlacklist(BLACKLIST);
+
+    @Override
+    public String getName() {
+        return "extras_ae2";
+    }
+
+    @Override
+    public Set<MaterialType> getMaterialTypes() {
+        return EnumSet.allOf(MaterialType.class);
+    }
+
+    @Override
+    public List<IFormRequest> getFormRequests() {
+        return Collections.singletonList(this.api.newFormRequest(this, this.circuitForm, this.processorForm));
+    }
+
+    @Override
+    public void onCommonSetup(IModuleData moduleData, FMLCommonSetupEvent event) {
+        IMiscHelper miscHelper = api.miscHelper();
+        Item press = ForgeRegistries.ITEMS.getValue(new ResourceLocation("ae2:engineering_processor_press"));
+
+        for (IMaterial material : circuitForm.getMaterials()) {
+            String name = material.getName();
+            if (!BLACKLIST.contains(name)) {
+                ResourceLocation materialLocation = miscHelper.getTagLocation(material.getType().getFormName(), name);
+                IItemInfo circuitInfo = api.itemFormType().getMaterialFormInfo(circuitForm, material);
+                api.registerRecipe(
+                        new ResourceLocation("jaopcaextras", "inscriber.circuit" + material.getName()),
+                        new AE2InscriberRecipeSerializer(
+                                "inscribe",
+                                materialLocation,
+                                press,
+                                circuitInfo
+                        )
+                );
+            }
+        }
+
+        for (IMaterial material : processorForm.getMaterials()) {
+            String name = material.getName();
+            if (!BLACKLIST.contains(name)) {
+                ResourceLocation circuitLocation = miscHelper.getTagLocation("circuits", material.getName());
+                IItemInfo processorInfo = api.itemFormType().getMaterialFormInfo(processorForm, material);
+                Item redstone = ForgeRegistries.ITEMS.getValue(new ResourceLocation("minecraft:redstone"));
+                api.registerRecipe(
+                        new ResourceLocation("jaopcaextras", "inscriber.processor." + material.getName()),
+                        new AE2InscriberRecipeSerializer(
+                                "press",
+                                redstone,
+                                circuitLocation,
+                                processorInfo
+                        )
+                );
+            }
+        }
+    }
+}
---
+++ src/main/java/io/github/bluesheep2804/jaopcaextras/recipes/AE2InscriberRecipeSerializer.java
@@ -0,0 +1,51 @@
+package io.github.bluesheep2804.jaopcaextras.recipes;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import thelm.jaopca.api.JAOPCAApi;
+import thelm.jaopca.api.helpers.IMiscHelper;
+import thelm.jaopca.api.recipes.IRecipeSerializer;
+
+public class AE2InscriberRecipeSerializer implements IRecipeSerializer {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public final String mode;
+    public final Object inputMiddle;
+    public final Object inputTop;
+    public final Object result;
+
+    public AE2InscriberRecipeSerializer(String mode, Object inputMiddle, Object inputTop, Object result) {
+        this.mode = mode;
+        this.inputMiddle = inputMiddle;
+        this.inputTop = inputTop;
+        this.result = result;
+    }
+
+    @Override
+    public JsonElement get() {
+        JAOPCAApi api = JAOPCAApi.instance();
+        IMiscHelper miscHelper = api.miscHelper();
+        JsonObject json = new JsonObject();
+        json.addProperty("type", "ae2:inscriber");
+        json.addProperty("mode", this.mode);
+
+        JsonObject ingredientsJson = new JsonObject();
+        ingredientsJson.add("middle", miscHelper.getIngredient(this.inputMiddle).toJson());
+        ingredientsJson.add("top", miscHelper.getIngredient(this.inputTop).toJson());
+        if (this.mode == "press") {
+            Item printedSilicon = ForgeRegistries.ITEMS.getValue(new ResourceLocation("ae2:printed_silicon"));
+            ingredientsJson.add("bottom", miscHelper.getIngredient(printedSilicon).toJson());
+        }
+        json.add("ingredients", ingredientsJson);
+
+        json.add("result", miscHelper.getIngredient(this.result).toJson());
+
+        LOGGER.info(json);
+        return json;
+    }
+}
