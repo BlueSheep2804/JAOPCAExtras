f793c75effd52061900a140a984a4184c1c743b8
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2023-09-13 18:06:35 +0900

    feat: 🎸 port to 1.20.1
    Extra press is not show in creative tabs...

--- src/main/java/io/github/bluesheep2804/jaopcaextras/init/ItemInit.java
+++ src/main/java/io/github/bluesheep2804/jaopcaextras/init/ItemInit.java
@@ -5,9 +5,8 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import thelm.jaopca.api.JAOPCAApi;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Jaopcaextras.MOD_ID);
-    public static RegistryObject<Item> EXTRA_PRESS = ITEMS.register("extra_press", () -> new Item(new Item.Properties().tab(JAOPCAApi.instance().creativeTab())));
+    public static RegistryObject<Item> EXTRA_PRESS = ITEMS.register("extra_press", () -> new Item(new Item.Properties()));
 }
